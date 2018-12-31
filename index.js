fe5ffff19a5e4c244765c0db2ecc4e337e33771b
TxHawks <tofu.hawks@gmail.com>
2018-12-31 19:04:15 +0200

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+const spawn = require('cross-spawn');
+// const path = require('path');
+const chalk = require('chalk');
+
+const validateConfig = require('./lib/validateConfig');
+const createRcFile = require('./lib/createRcFile');
+const getProjectPath = require('./lib/getProjectPath');
+const autocomplete = require('./lib/autocomplete');
+
+htz();
+
+async function htz() {
+  await autocomplete();
+
+  const args = process.argv.slice(2);
+  const command = args[0];
+
+  if (!command) {
+    const projectPath = await validateConfig();
+    process.exitCode = 0;
+    return projectPath;
+  }
+  if (command === '--setup') {
+    return undefined;
+  }
+  // if (command === 'create-config') {
+  //   const projectPath = await createRcFile(true);
+  //   process.exitCode = 0;
+  //   return projectPath;
+  // }
+  if (command === 'reconfigure') {
+    const projectPath = await createRcFile(true);
+    process.exitCode = 0;
+    return projectPath;
+  }
+  if (command === 'cwd') {
+    const [ , action, ...restArgs ] = args;
+    return runTask(action, restArgs, process.cwd());
+  }
+
+  const projectPath = getProjectPath();
+  if (!projectPath) {
+    console.error(
+      chalk`{red "htz" requires the project dir to be configured in ~/.config/.htzrc.js}\n`,
+      'Alternatively, you can use "htz cwd <command>" to run commands in the CWD.'
+    );
+    process.exitCode = 1;
+  }
+
+  const [ action, ...restArgs ] = args;
+  runTask(action, restArgs, projectPath);
+
+  return undefined;
+}
+
+function runTask(action, args, cwd) {
+  const isGlobalAction = [ 'add', 'remove', 'run', ].includes(action);
+  const yarnCmd = isGlobalAction ? 'workspaces' : 'workspace';
+  const cmd = isGlobalAction ? action : `@haaretz/${action}`;
+
+  const yarnWorkspace = spawn('cross-env', [ 'yarn', yarnCmd, cmd, ...args, ], {
+    cwd,
+    stdio: 'inherit',
+  });
+
+  yarnWorkspace.on('close', code => {
+    process.exitCode = code;
+  });
+}
